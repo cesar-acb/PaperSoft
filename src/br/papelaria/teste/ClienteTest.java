f958088b18d9a52f42f6871165337ea06fc5236c
cesar-acb <cesar-acb@hotmail.com>
2016-10-02 20:15:15 -0300

    Pasta de teste

---
+++ src/br/papelaria/teste/ClienteTest.java
@@ -0,0 +1,44 @@
+package br.papelaria.testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClienteTest {
+
+	@Test
+	public final void testIncluirCliente() {
+		System.out.println("\nTeste de Inclusao de Cliente Executado com sucesso!\n");
+	}
+
+	@Test
+	public final void testAlterarCliente() {
+		System.out.println("\nTeste de Alteracao de Cliente Executado com sucesso!\n");
+	}
+
+	@Test
+	public final void testConsultaCliente() {
+		System.out.println("\nTeste de Consulta de Cliente Executado com sucesso!\n");
+	}
+
+	@Test
+	public final void testExcluirCliente() {
+		System.out.println("\nTeste de Exclusao de Cliente Executado com sucesso!\n");
+	}
+
+	@Test
+	public final void testCadastrar() {
+		System.out.println("\nTeste de Cadastro Executado com sucesso!\n");
+	}
+
+	@Test
+	public final void testAtualizar() {
+		System.out.println("\nTeste de Atualizacao Executado com sucesso!\n");
+	}
+
+	@Test
+	public final void testApagar() {
+		System.out.println("\nTeste de Exclusao Executado com sucesso!\n");
+	}
+
+}
